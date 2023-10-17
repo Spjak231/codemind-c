9c3345e20c18fc44df648797676ecd668ad8f922
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-10-17 23:02:34 +0530

    #include<stdio.h> int main(){     int x,y;     scanf("%d%d",&x,&y);     if(x>y){         printf("%d",x-y);     }     else if(y>x){         printf("%d",y-x);     }     else{
        }
    }

--- Donation.c
+++ Donation.c
@@ -2,11 +2,11 @@
 int main(){
     int x,y;
     scanf("%d%d",&x,&y);
-    if(y>x){
-        printf("%d",y-x);
-    }
-    else if(x>y){
+    if(x>y){
         printf("%d",x-y);
+    }
+    else if(y>x){
+        printf("%d",y-x);
     }
     else{
         
