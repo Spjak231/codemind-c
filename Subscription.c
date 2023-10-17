3b61f7bc9a2c82dcb52f0efbf681bf1cc0bb8232
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-10-17 23:11:23 +0530

    #include<stdio.h> int main(){     int x;     scanf("%d",&x);     if(x>40){         printf("YES");         }     else if(x<=40){         printf("NO");     }     else{
        }
    }

---
+++ Subscription.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    if(x>40){
+        printf("YES");
+        }
+    else if(x<=40){
+        printf("NO");
+    }
+    else{
+        
+    }
+}
