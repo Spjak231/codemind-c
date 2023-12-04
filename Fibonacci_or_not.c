359656d0c50ab4f69b25e5223f3647364210327a
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-04 23:12:39 +0530

    #include<stdio.h> int main(){ 	int n,f1=0,f2=1,f3=1; 	scanf("%d",&n); 	if(f1==n){ 		printf("True"); 	} 	else{ 		f3=f1+f2; 		while(f3<n){ 			f1=f2; 			f2=f3; 			f3=f1+f2; 		} 		if(f3==n){ 			printf("True"); 		} 		else{ 			printf("False"); 		} 	}
    }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int n,f1=0,f2=1,f3=1;
+	scanf("%d",&n);
+	if(f1==n){
+		printf("True");	
+	}
+	else{
+		f3=f1+f2;
+		while(f3<n){
+			f1=f2;
+			f2=f3;
+			f3=f1+f2;
+		}
+		if(f3==n){
+			printf("True");
+		}
+		else{
+			printf("False");
+		}
+	}
+	
+}
