298f0e65acf2f3561652d8bdc8fb084948be749b
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-11 15:27:06 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     for(int row=1;row<=n;row++){         for(int col=1;col<=row;col++){             int alp=65+n-row;             char d=(char)alp;             printf("%c ",d);         }
            printf("
    ");
        }
    }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            int alp=65+n-row;
+            char d=(char)alp;
+            printf("%c ",d);
+        }
+        
+        printf("
+");
+    }
+}
