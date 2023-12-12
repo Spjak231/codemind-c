cd818a0bc2efe44b207462bd5a093ab8db0447cc
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-13 00:52:57 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int temp = n;     for(int row=1;row<=n;row++){         for(int col=1;col<=n;col++){             if((row==col) || (row+col==n+1)){             //if((row==col) || (row==1 && col==n) || (col==1 && row==n) || (row+col==n+1)){                 printf("%d",temp);             }             else{                 printf(" ");             }
            }
            temp--;
            printf("
    ");
        }
    
    }

---
+++ Pattern_8.3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int temp = n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            if((row==col) || (row+col==n+1)){
+            //if((row==col) || (row==1 && col==n) || (col==1 && row==n) || (row+col==n+1)){
+                printf("%d",temp);
+            }
+            else{
+                printf(" ");
+            }
+            
+        }
+        temp--;
+        printf("
+");
+    }
+
+}
