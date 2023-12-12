ecf4bfdb22b17a6df65705f4c53495c17a1f793f
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-13 00:27:37 +0530

    #include<stdio.h> int main(){     int n;
        scanf("%d",&n);
        int a=n;
        for(int row=1;row<=n;row++){
            for(int col_space=1;col_space<=row-1;col_space++){
                printf(" ");
            }
            for(int col_num=1;col_num<=n+1-row;col_num++){
                
                printf("%d ",a);
            }
            a--;
            
            printf("
    ");
        }
    }

---
+++ Pattern_7.2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    
+    scanf("%d",&n);
+    int a=n;
+    for(int row=1;row<=n;row++){
+        for(int col_space=1;col_space<=row-1;col_space++){
+            printf(" ");
+        }
+        for(int col_num=1;col_num<=n+1-row;col_num++){
+            
+            printf("%d ",a);
+        }
+        a--;
+        
+        printf("
+");
+    }
+}
