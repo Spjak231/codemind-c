556cd2a21979f1f7103d559f0d831086ffc56ab5
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-19 14:14:41 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int sum1=0,sum2=0;     for(int i=0;i<n;i++){         if(arr[i]%2==0){             sum1=sum1+arr[i];         }         else{             sum2=sum2+arr[i];         }     }     int d;     (sum1>sum2) ? d=sum1-sum2 : d=sum2-sum1;     printf("%d",d); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum1=0,sum2=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            sum1=sum1+arr[i];
+        }
+        else{
+            sum2=sum2+arr[i];
+        }
+    }
+    int d;
+    (sum1>sum2) ? d=sum1-sum2 : d=sum2-sum1;
+    printf("%d",d);
+}
