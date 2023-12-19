ec43483eba88fe1826859b73b6e184656ca5a10a
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-19 22:04:27 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int count=0;     for(int i=1;i<n;i++){         if(arr[i]%2==0){             if(arr[i-1]%2!=0 && arr[i+1]%2!=0){                 count++;             }         }     }     printf("%d",count); }

---
+++ Count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count=0;
+    for(int i=1;i<n;i++){
+        if(arr[i]%2==0){
+            if(arr[i-1]%2!=0 && arr[i+1]%2!=0){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
