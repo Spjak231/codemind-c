10a0075284187b91650c92ceb5f76b83cdcd29e9
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-19 11:45:05 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int min=arr[0];     for(int i=0;i<n;i++){         if(min>arr[i]){             min=arr[i];         }     }     printf("%d",min); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(min>arr[i]){
+            min=arr[i];
+        }
+    }
+    printf("%d",min);
+}
