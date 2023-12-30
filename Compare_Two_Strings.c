404b5ef29b42501d47b397013567224d949b76bd
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-31 02:30:40 +0530

    #include<stdio.h> #include<string.h> int main(){     char s1[100],s2[100];     scanf("%[^ ]s",s1);     scanf(" %[^ ]s",s2);     int comp=strcmp(s1,s2);     if(comp==0){         printf("Strings are Equal");     }     else{          printf("Strings are not Equal");     } }

---
+++ Compare_Two_Strings.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s1[100],s2[100];
+    scanf("%[^
+]s",s1);
+    scanf(" %[^
+]s",s2);
+    int comp=strcmp(s1,s2);
+    if(comp==0){
+        printf("Strings are Equal");
+    }
+    else{
+         printf("Strings are not Equal");
+    }
+}
