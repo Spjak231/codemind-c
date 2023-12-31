d33003a3c4b8abf1e9fdddf79d91e09cbacf683f
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2023-12-31 12:49:49 +0530

    #include<stdio.h> #include<string.h> int main(){     char S[100];     scanf("%[^ ]s",S);     int count=0;     for(int i=0;S[i]!=NULL;i++){         if(S[i]>=97 && S[i]<=122){             count++;         }     }     printf("%d",count); }

---
+++ Lowercase_Letters_in_a_String.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char S[100];
+    scanf("%[^
+]s",S);
+    int count=0;
+    for(int i=0;S[i]!=NULL;i++){
+        if(S[i]>=97 && S[i]<=122){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
